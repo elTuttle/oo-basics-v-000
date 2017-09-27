609f6f33d1b62ae31c29d6f156fb6bbf4cefcde8
John Tuttle <thejwt@gmail.com>
2017-09-27 19:42:31 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,4 +3,12 @@ class Book
   def initialize(title)
     @title = title
   end
+
+  def properties
+    attr_accessor :author, :page_count, :genres
+  end
+
+  def turn_page
+    puts "page turned."
+  end
 end
